2f2797a139fbe62c1817394ffc6eb3bacd499447
Emre <emreeozturk42@gmail.com>
2023-04-22 05:43:08 +0300

    project part 1 finished

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -3,10 +3,93 @@ import { client, urlFor } from "@/sanity";
 import Header from "@/components/header/Header";
 import { Post } from "@/typings";
 import { GetStaticProps } from "next";
-const Post = () => {
+import Image from "next/image";
+const Post = ({ post: Post }: { post: Post }) => {
+  console.log(Post);
   return (
     <main>
       <Header />
+      <Image
+        className="object-cover w-full h-96"
+        src={urlFor(Post.mainImage).url()!}
+        alt={Post.title}
+        width={1000}
+        height={500}
+      />
+      <article
+        className="
+        max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8 text-center
+        "
+      >
+        <h1 className="text-4xl font-bold  my-4 text-gray-800 ">
+          {Post.title}
+        </h1>
+        <h2 className="text-xl font-light">
+          <p className="text-gray-800  my-4">{Post.description}</p>
+        </h2>
+        <div className="flex items-center justify-center">
+          <div className="flex items-center border-2 rounded-md p-1">
+            <Image
+              className="rounded-full"
+              src={urlFor(Post.author.image).url()!}
+              alt={Post.author.name}
+              width={50}
+              height={50}
+            />
+            <p className="text-gray-800 font-extralight ml-2">
+              Post Author:{" "}
+              <span className="text-green-500 font-semibold">
+                {Post.author.name}
+              </span>
+              <br />
+              Published at: {new Date(Post._createdAt).toLocaleString()}
+            </p>
+          </div>
+        </div>
+        <div
+          className="
+        max-w-2xl mx-auto my-8
+        
+        "
+        >
+          <div>
+            {Post.body.map((block: any) => {
+              if (block._type === "image") {
+                return (
+                  <div key={block._key}>
+                    <Image
+                      src={urlFor(block.asset).url()!}
+                      alt={block.alt}
+                      width={300}
+                      height={300}
+                    />
+                    <p className="text-gray-800 font-extralight text-center">
+                      {block.caption}
+                    </p>
+                  </div>
+                );
+              }
+              return (
+                <div key={block._key}>
+                  {block.children.map((child: any) => {
+                    if (child._type === "span") {
+                      return (
+                        <span
+                          key={child._key}
+                          className={child.marks.map((mark: any) => mark)}
+                        >
+                          {child.text}
+                        </span>
+                      );
+                    }
+                    return <p key={child._key}>{child.text}</p>;
+                  })}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </article>
     </main>
   );
 };
@@ -44,13 +127,20 @@ export const getStaticProps: GetStaticProps = async (context: any) => {
         description,
         slug,
         mainImage,
+        _createdAt,
         body,
         'comments': *[_type == "comment" && post._ref == ^._id && approved == true]
       }`;
   const post = await client.fetch(query, { slug });
+  if (!post)
+    return {
+      notFound: true,
+    };
+
   return {
     props: {
       post,
     },
+    revalidate: 60,
   };
 };
